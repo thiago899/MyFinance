d733b325e6284e59158602b4e24c3fbdd75877db
Thiago <thiagoonline89@hotmail.com>
2018-09-27 18:35:36 -0300

    Desenvolvido parte do registro de transações financeiras.

--- MyFinance/Controllers/TransacaoController.cs
+++ MyFinance/Controllers/TransacaoController.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFinance.Models;
 
 namespace MyFinance.Controllers
 {
     public class TransacaoController : Controller
     {
+        IHttpContextAccessor HttpContextAccessor;
+
+        public TransacaoController(IHttpContextAccessor httpContextAcessor)
+        {
+            HttpContextAccessor = httpContextAcessor;
+        }
+
         public IActionResult Index()
         {
+            TransacaoModel objTransacao = new TransacaoModel(HttpContextAccessor);
+            ViewBag.ListaTransacao = objTransacao.ListaTransacao();
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Registrar(TransacaoModel formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                formulario.HttpContextAccessor = HttpContextAccessor;
+                formulario.Insert();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registrar(int? id)
+        {
+            if (id != null)
+            {
+                TransacaoModel objTransacao = new TransacaoModel(HttpContextAccessor);
+                ViewBag.Registro = objTransacao.CarregarRegistro(id);
+            }
+            return View();
+        }
         public IActionResult Extrato()
         {
             return View();
---
+++ MyFinance/Models/transacaoModel.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using MyFinance.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFinance.Models
+{
+    public class TransacaoModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe a Data")]
+        public string Data { get; set; }
+        public string Tipo { get; set; }
+        public double Valor{ get; set; }
+        [Required(ErrorMessage = "Informe a Descrição")]
+        public string Descricao { get; set; }
+
+        public int Conta_Id { get; set; }
+
+        public string NomeConta { get; set; }
+
+        public int Plano_Contas_Id { get; set; }
+        public string DescricaoPlanoConta { get; set; }
+
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public TransacaoModel()
+        {
+
+        }
+
+        //Recebe o contexto para acesso ás variáveis de sessão.
+        public TransacaoModel(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public List<TransacaoModel> ListaTransacao()
+        {
+            List<TransacaoModel> lista = new List<TransacaoModel>();
+            TransacaoModel item;
+
+            string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
+            string sql = "Select t.Id, t.Data, t.Tipo, t.Valor, t.Descricao as historico," +
+                         " t.Conta_Id, c.Nome as Conta, t.Plano_Contas_Id, p.Descricao as Plano_Conta" +
+                         " from Transacao as t inner join Conta c" +
+                         " on t.Conta_Id = c.Id inner join Plano_Contas as p " +
+                         " on t.Plano_Contas_Id = p.Id" +
+                         $" where t.Usuario_Id = {id_usuario_logado} order by t.data desc limit 10";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new TransacaoModel();
+                item.Id = int.Parse(dt.Rows[i]["ID"].ToString());
+                item.Data = DateTime.Parse(dt.Rows[i]["DATA"].ToString()).ToString("dd/MM/yyy");
+                item.Tipo = dt.Rows[i]["Tipo"].ToString();
+                item.Descricao = dt.Rows[i]["historico"].ToString();
+                item.Valor = double.Parse(dt.Rows[i]["Valor"].ToString());
+                item.Conta_Id = int.Parse(dt.Rows[i]["Conta_Id"].ToString());
+                item.NomeConta = dt.Rows[i]["conta"].ToString();
+                item.Plano_Contas_Id = int.Parse(dt.Rows[i]["Plano_Contas_Id"].ToString());
+                item.DescricaoPlanoConta = dt.Rows[i]["plano_conta"].ToString();
+
+                lista.Add(item);
+            }
+            return lista;
+
+        }
+    }
+}
